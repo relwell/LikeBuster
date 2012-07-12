378e3c51ff48c0ae468271cb697dd3e5cc1db178
Robert Elwell <robert@wikia-inc.com>
2012-07-12 15:31:57 -0700

    update script to leave photo, status, and link likes alone

--- LikeBuster.js
+++ LikeBuster.js
@@ -3,7 +3,7 @@ jQuery(function() {
 		jQuery('h6.uiStreamPassive').each(function(){
 			var h6 = jQuery(this);
 			var li = h6.closest('li');
-			if (h6.text().match(/ likes /)) {
+			if (h6.text().match(/ likes [^(a photo)(a status)(a link)]/)) {
 			    console.log("Hid some junk: "+h6.text()+" -- Did you really want to see that?");
 			    li.remove();
 			}
