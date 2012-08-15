33a8fbf866dab9358c3f9b2e8ab4ba7ddb6d6ad3
Robert Elwell <robert@wikia-inc.com>
2012-08-15 15:04:32 -0700

    Add support for "so-and-so claimed an offer"

--- LikeBuster.js
+++ LikeBuster.js
@@ -3,8 +3,11 @@ jQuery(function() {
 		jQuery('h6.uiStreamPassive').each(function(){
 			var h6 = jQuery(this);
 			var li = h6.closest('li');
-			if (h6.text().match(/ like(s)? [^(a photo)(a status)(a link)]/)) {
-			    console.log("Hid some junk: "+h6.text()+" -- Did you really want to see that?");
+			var text = h6.text();
+
+			if (text.match(/ like(s)? [^(a photo)(a status)(a link)]/)
+			    || text.match(/ claimed an offer /)) {
+			    console.log("Hid some junk: "+text+" -- Did you really want to see that?");
 			    li.remove();
 			}
 		});
