7ea56db3bdde08e5c5a024013266e91c8f248595
Robert Elwell <robert@wikia-inc.com>
2012-08-27 13:47:30 -0700

    Added support for "sponsored" tag, included in things like the "Lays Do Us a Flavor" ad campaign, with variable predicate text

--- LikeBuster.js
+++ LikeBuster.js
@@ -1,13 +1,28 @@
+function logRemove(text)
+{
+    console.log("Hid some junk: "+text+" -- Did you really want to see that?");
+}
+
 jQuery(function() {
 	setInterval(function() {
 		jQuery('h6.uiStreamPassive').each(function(){
 			var h6 = jQuery(this);
-			var li = h6.closest('li');
 			var text = h6.text();
 
 			if (text.match(/ like(s)? [^(a photo)(a status)(a link)]/)
 			    || text.match(/ claimed an offer /)) {
-			    console.log("Hid some junk: "+text+" -- Did you really want to see that?");
+			    var li = h6.closest('li');
+			    logRemove(text);
+			    li.remove();
+			}
+		});
+		jQuery('span.uiStreamAdditionalLogging').each(function(){
+			var span = jQuery(this);
+			var text = span.text();
+			
+			if (text.match(/^Sponsored$/)) {
+			    var li = span.closest('li');
+			    logRemove(li.find('h6').text());
 			    li.remove();
 			}
 		});
