ca3f13dcc1ee0c8e365cf65974d63c4de1d19a46
chriswoodrich <cwoodrich@gmail.com>
2014-04-11 10:27:04 -0700

    DRYer code per @relwell 's suggestions

--- LikeBuster.js
+++ LikeBuster.js
@@ -68,21 +68,7 @@ var likeBust = function() {
 			logRemove(text);
 			li.remove();
 			});
-		jQuery('a.shareText[href*=buzzfeed]').each(function() {
-			var a = jQuery(this);
-			var text = a.find('strong').text();
-			logRemove(text);
-			console.log(a.closest('li.genericStreamStory'));
-			a.closest('li.genericStreamStory').remove();
-		    });
-		jQuery('a.shareText[href*=upworthy]').each(function() {
-			var a = jQuery(this);
-			var text = a.find('strong').text();
-			logRemove(text);
-			console.log(a.closest('li.genericStreamStory'));
-			a.closest('li.genericStreamStory').remove();
-		    });
-		jQuery('a.shareText[href*=medium]').each(function() {
+		jQuery('a.shareText[href*=buzzfeed], a.shareText[href*=upworthy], a.shareText[href*=medium]').each(function() {
 			var a = jQuery(this);
 			var text = a.find('strong').text();
 			logRemove(text);
